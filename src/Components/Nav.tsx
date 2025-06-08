e2017030e5d4d46d74035ce0882ab1ed295fd6d9
Firdavs Komilov <firdavskomilov05@gmail.com>
2025-06-08 11:10:10 +0500

    first commit

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Nav: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
- 
+
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
@@ -16,26 +16,7 @@ const Nav: React.FC = () => {
           <div className="nav_box">
             <Link to="/" className="nav_logo">Exclusive</Link>
 
-            <div className="nav">
-            <button
-              type="button"
-              className="header-menu-button"
-              onClick={toggleMenu}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="currentColor"
-              >
-                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-              </svg>
-            </button>
-            </div>
-
-            <ul className={`nav_list ${menuOpen ? 'open' : ''}`}>
-            
+            <ul className="nav_list">
               <li>
                 <NavLink
                   to="/"
